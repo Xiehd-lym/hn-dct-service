ecf099ead97168c3bbe2f64c136597184d3e66c7
Xiehd <xiehdwork@163.com>
2023-04-01 23:55:49 +0800

    1

--- src/test/java/com/ahzx/hndctservice/HnDctServiceApplicationTests.java
+++ src/test/java/com/ahzx/hndctservice/HnDctServiceApplicationTests.java
@@ -3,6 +3,7 @@ package com.ahzx.hndctservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//
 @SpringBootTest
 class HnDctServiceApplicationTests {
 
